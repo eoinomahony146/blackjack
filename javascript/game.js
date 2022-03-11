7a62ff0222e82969afea31370ca97deab352daa3
eoinomahony146 <eoinomahony146@gmail.com>
2022-03-11 18:30:43 +0000

    Updated functions, implemented logic of game, added comments

--- javascript/game.js
+++ javascript/game.js
@@ -1,63 +1,121 @@
-var suits = ["S", "H", "D", "C"];
-var facevalues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-var deck = new Array();
+// Waits for html and css to load before executing startGame()
+window.addEventListener('DOMContentLoaded', init, false);
+
+let suits = ["s", "h", "d", "c"];
+let facevalues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a"];
+let deck = new Array();
+let dealerCards = new Array();
+let dealerScore = 0;
+let playerCards = new Array();
+let playerScore = 0;
+function init() {
+
+    // find by id #btnStart button and attach it an event listener
+    let btnStart = document.querySelector("#btnStart");
+    btnStart.addEventListener( "click", startGame, false );
+    btnHit.addEventListener( "click", Hit, false );
+    btnStand.addEventListener( "click", Stand, false );
+}
+
+function startGame() {
+    // function to initialize the game  
+    createDeck();
+    shuffle(); 
+    deal();
+}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function createDeck() {
-    deck = new Array();
-    for (var i = 0; i < facevalues.length; i++) {
-        for (var x = 0; x < suits.length; x++) {
-            var weight = parseInt(facevalues[i]);
-            if (facevalues[i] == "J" || facevalues[i] == "Q" || facevalues[i] == "K")
+    // function that creates and assignes weights to cards in the deck
+    deck = new Array(); // reset deck
+
+    for (let i = 0; i < facevalues.length; i++) {
+        for (let x = 0; x < suits.length; x++) {
+            let weight = parseInt(facevalues[i]);
+            if (facevalues[i] == "j" || facevalues[i] == "q" || facevalues[i] == "k")
                 weight = 10;
-            if (facevalues[i] == "A")
-                weight = 11 || 1;
-            var card = { Value: facevalues[i], Suit: suits[x], Weight: weight };
+            if (facevalues[i] == "a")
+                weight = 11;
+            let card = { Value: facevalues[i], Suit: suits[x], Weight: weight };
             deck.push(card);
         }
     }
 }
 
-function shuffle(timesShufflfed) {
-    for(var i = 0; i < timesShufflfed; i++) {
-        var rndNo = getRandomInt(1,52);
-        var card = deck[i]
-        deck[i] = card[rndNo];
+function shuffle() {
+    // function that shuffles the created deck by swapping a card at a given 
+    // index with a random card in the deck
+    for(let i = 0; i < 52; i++) {
+        let rndNo = getRandomInt(1,52);
+        let card = deck[i];
+        
+        deck[i] = deck[rndNo];
         deck[rndNo] = card;
     }
 }
 
-
-playersList = new Array();
-
-function players() {
-    var player1 =  document.getElementById('user1').value;
-    var player2 =  document.getElementById('user2').value;
-    var player3 =  document.getElementById('user3').value;
-    var player4 =  document.getElementById('user4').value;
-    if( player1 != "" || player2 != "" || player3 != "" || player4 != "") {
-
-        playersList.push(player1)
-        playersList.push(player2)
-        playersList.push(player3)
-        playersList.push(player4)
-        console.log(playersList)
-    }
-    console.log("User1: "+ player1)
-    console.log("User2: "+ player2)
-    console.log("User3: "+ player3)
-    console.log("User4: "+ player4)
+function deal() {
+    // function that initially deals the player 2 cards, and the dealer one.
+    playerCards = [nextCard(), nextCard()]
+    dealerCards = [nextCard()]
+    Score();
 }
 
-// function deal(num) {
-//     player1
-// }
+function nextCard() {
+    // function that returns the next available card from the deck and removes it from the deck
+    return deck.shift();
+}
 
+function Hit() {
+    // function that deals the player another card and recalculates their score
+    playerScore = 0;
+    playerCards.push(deck.shift());
+    Score();
+}
 
-createDeck();
-console.log(deck);
-shuffle(getRandomInt(1, 52))
-console.log(deck);
+function Stand() {
+    // function that deals the dealer another card
+    if (dealerScore < 21)
+        dealerCards.push(deck.shift());
+        Score();
+} 
+
+function Score() {
+    // logic of blackjack implemented 
+    playerScore = 0;
+    for(i = 0; i < playerCards.length; i ++) {
+        playerScore += playerCards[i].Weight;
+        console.log(playerCards);
+        console.log("Player Score: " + playerScore);
+    }
+
+    dealerScore = 0;
+    for(i = 0; i < dealerCards.length; i ++) {
+        dealerScore += dealerCards[i].Weight;
+        console.log(dealerCards);
+        console.log("Dealer Score: " + dealerScore);
+    }
+    
+    if (playerScore > 21) {
+        // change the value of "A" from 11 if the score is over 21 to 1.
+        for (i = 0; i < playerCards.length; i ++) {
+            if (playerCards[i].Value == "a" && playerCards[i].Weight == 11) {
+                playerCards[i].Weight == 1;
+                playerScore += 1
+            } else {
+                console.log("Dealer wins!")
+            }
+        }
+    } if (dealerScore > 21) {
+        console.log("Player wins")
+    } if (playerScore == dealerScore) {
+        console.log("Tie!")
+    } if (dealerScore == 21 && playerScore != 21) {
+        console.log("Dealer wins")
+    } if (playerScore == 21 && dealerScore != 21) {
+        console.log("Player wins")
+    }
+}
